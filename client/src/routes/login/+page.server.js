1422d5467895a3241d442e68956b8b9e757846b0
cowbra <32939303+cowbra@users.noreply.github.com>
2023-02-07 13:34:35 +0100

    Login page

---
+++ client/src/routes/login/+page.server.js
@@ -0,0 +1,30 @@
+import { fail, redirect } from '@sveltejs/kit';
+import * as api from '$lib/api.js';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ parent, url }) {
+  const { user } = await parent();
+  if (user) return redirect(307, '/');
+  const para = url.searchParams.get('error');
+  return { para };
+}
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+  default: async ({ cookies, request }) => {
+    const data = await request.formData();
+    const user = {
+      username: data.get('username'),
+      password: data.get('password'),
+    };
+
+    const { errors, token } = await api.post('users/login', user);
+
+    if (errors) {
+      return fail(401, { errors });
+    }
+
+    cookies.set('jwt', token, { path: '/' });
+    return redirect(307, '/locations');
+  },
+};
---
+++ client/src/routes/login/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+    import { enhance } from '$app/forms';
+    /**
+     * @type {{ para: any; }}
+     */
+     export let data;
+</script>
+
+<body class="login">
+    {#if data.para}
+      <p class="error-message">Incorrect Password</p>
+    {/if}
+  
+    <div class="login-form">
+      <h1>Login</h1>
+      <form use:enhance method="POST">
+        <div class="content">
+          <div class="input-field">
+            <input
+              name="username"
+              type="text"
+              required
+              placeholder="Username"
+            />
+          </div>
+          <div class="input-field">
+            <input
+              name="password"
+              type="password"
+              required
+              placeholder="Password"
+            />
+          </div>
+        </div>
+        <div class="action">
+          <button type="submit">Sign up</button>
+          <button on:click={() => window.location.href='/register'}>Register</button>
+        </div>
+      </form>
+    </div>
+  </body>
+  
